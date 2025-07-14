272b3253a03359f46fd51f0b1080c9f8f5dcf03d
Anirudh Acharya <anirudhra@hotmail.com>
2025-07-14 09:28:51 -0700

    1) Updated tmux shortcuts and remap file name 2) Added git update force function to .gitfuncs instead of individual    update.sh files 3) Update router aliases to remove vim - nano alias

--- linux/gtk/themes/elementary-sounds/update.sh
+++ linux/gtk/themes/elementary-sounds/update.sh
@@ -17,20 +17,4 @@ installupdate() {
 # Source the git functions (now includes run_git_update_and_install)
 source "${HOME}/.gitfuncs"
 
-force_update="false"
-if [ $# -gt 0 ]; then
-  case "$1" in
-    --force)
-      force_update="true"
-      ;;
-    --help)
-      print_git_theme_update_help
-      ;;
-    *)
-      print_git_theme_update_help
-      ;;
-  esac
-fi
-
-# Run the common logic
-run_git_update_and_install "$giturl" "$gitdir" installupdate "$force_update"
+run_git_update_and_install "$giturl" "$gitdir" installupdate "$@"
