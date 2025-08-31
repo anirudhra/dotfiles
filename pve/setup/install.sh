1d6e771575140ea694cf0ec564744355354b1b8d
Anirudh Acharya <anirudhra@hotmail.com>
2025-08-31 15:39:30 -0700

    fixed xset install issue (removed)

--- pve/setup/install.sh
+++ pve/setup/install.sh
@@ -86,7 +86,6 @@ pve_packages=(
   'iperf3'
   'iptraf-ng'
   'linux-cpupower'
-  'xset'
   #'intel-media-va-driver-non-free' # this needs to be installed after enabling non-free repo in /etc/apt/sources.list.d/debian
   'vainfo'
   'intel-gpu-tools'
