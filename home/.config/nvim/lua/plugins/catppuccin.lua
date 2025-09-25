5922b15bb67b6cf7c07ff43ddb7ac48af0234f7d
Anirudh Acharya <anirudhra@hotmail.com>
2025-09-25 07:04:13 -0700

    lazyvim plugin config fixes

--- home/.config/nvim/lua/plugins/catppuccin.lua
+++ home/.config/nvim/lua/plugins/catppuccin.lua
@@ -5,13 +5,30 @@ return {
     lazy = false,
     priority = 1000,
 
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-      if module then
-        module.get = module.get_theme
-      end
-      return opts
-    end,
+    -- opts = function(_, opts)
+    --   local module = require("catppuccin.groups.integrations.bufferline")
+    --   if module then
+    --     module.get = module.get_theme
+    --   end
+    --   return opts
+    -- end,
+
+    -- until lazyvim fixes the get() to get_theme() method, temp fix
+    {
+      "akinsho/bufferline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if (vim.g.colors_name or ""):find("catppuccin") then
+          local ok, mod = pcall(require, "catppuccin.groups.integrations.bufferline")
+          if ok then
+            local get = mod.get_theme or mod.get
+            if type(get) == "function" then
+              opts.highlights = get({})
+            end
+          end
+        end
+      end,
+    },
 
     config = function()
       require("catppuccin").setup()
