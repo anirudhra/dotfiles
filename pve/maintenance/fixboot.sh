bf39f277add0ac252399233111e8fb57df97d69d
Anirudh Acharya <anirudhra@hotmail.com>
2026-01-10 10:13:56 -0800

    pve fixboot efi script

---
+++ pve/maintenance/fixboot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+proxmox-boot-tool status # to check if ESP is detected, if not then run:
+lsblk -o +FSTYPE         # look for /dev/xxx boot efi partition with type vfat
+umount /boot/efi
+proxmox-boot-tool init /dev/nvme1n1p2 # replace 'xxx' with the partition from the lsblk command above
+mount -a
+proxmox-boot-tool refresh
+proxmox-boot-tool status
