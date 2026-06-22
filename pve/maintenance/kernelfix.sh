58a2b0ce792d30f0cae5d4fdbb78b7b84f6938fd
Anirudh Acharya <anirudhra@hotmail.com>
2026-06-22 08:46:14 -0700

    PVE kernel update fix script updates

--- pve/maintenance/kernelfix.sh
+++ pve/maintenance/kernelfix.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-proxmox-boot-tool status # to check if ESP is detected, if not then run:
-lsblk -o +FSTYPE         # look for /dev/xxx boot efi partition with type vfat
-umount /boot/efi
-proxmox-boot-tool init /dev/nvme0n1p2 # replace 'xxx' with the partition from the lsblk command above
-mount -a
+##
+## following are the new fixes for kernel add/remove errors
+##
+echo 'grub-efi-amd64 grub2/force_efi_extra_removable boolean true' | debconf-set-selections -v -u
+apt install --reinstall grub-efi-amd64
 proxmox-boot-tool refresh
 proxmox-boot-tool status
+##
+## Following are the older suggestions for fixes, which are not very consistent, disabling
+##
+#proxmox-boot-tool status # to check if ESP is detected, if not then run:
+#lsblk -o +FSTYPE         # look for /dev/xxx boot efi partition with type vfat
+#umount /boot/efi
+#proxmox-boot-tool init /dev/nvme0n1p2 # replace 'xxx' with the partition from the lsblk command above
+#mount -a
+#proxmox-boot-tool refresh
+#proxmox-boot-tool status
